e8b2cb24ee26060fa1e86926b591b41c9d4bb581
amit2609 <amity2609@gmail.com>
2021-11-14 07:31:11 +0200

    add dbcontext

---
+++ ArticlesServerBL/ModelsBL/ArtiFindDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using ArticlesServerBL.Models;
+
+namespace ArticlesServerBL.ModelsBL
+{
+    public partial class ArtiFindDBContext: DbContext
+    {
+        
+    }
+}
