c468c6ab1abaa2890f9bf842da4e5bb8b7d9348c
amit2609 <amity2609@gmail.com>
2022-02-06 18:33:42 +0200

    dfg

--- ArticlesServer/Controllers/ArtiFindController.cs
+++ ArticlesServer/Controllers/ArtiFindController.cs
@@ -73,26 +73,11 @@ namespace ArticlesServer.Controllers
                 Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
             else
                 Response.StatusCode = (int)System.Net.HttpStatusCode.Forbidden;
-            Shuffle(list);
+           
             return list;
         }
 
-        private void Shuffle( List<Interest> list)
-        {
-            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
-            int n = list.Count;
-            while (n > 1)
-            {
-                byte[] box = new byte[1];
-                do provider.GetBytes(box);
-                while (!(box[0] < n * (Byte.MaxValue / n)));
-                int k = (box[0] % n);
-                n--;
-                Interest value = list[k];
-                list[k] = list[n];
-                list[n] = value;
-            }
-        }
+        
         //[Route("SendResetCode")]
         //[HttpGet]
         //public User CheckEmailAndSentCode([FromQuery] string email)
