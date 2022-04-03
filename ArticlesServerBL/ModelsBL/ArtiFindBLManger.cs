8462d63fb8c84ae326aa9675adc163df1c6c8e2c
User <User@LAB3-15>
2022-04-03 12:14:12 +0300

    fdsfdsf

---
+++ ArticlesServerBL/ModelsBL/ArtiFindBLManger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticlesServerBL.ModelsBL
+{
+    class ArtiFindBLManger
+    {
+        //scaffold-dbcontext "Server=localhost\sqlexpress;Database=ArtiFind;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -force
+        //Server=localhost\sqlexpress;Database=ArtiFindDB;Trusted_Connection=True;
+    }
+}
